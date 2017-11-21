66c8f36a040bea3e026d79222d3a3466aec9eb2c
EllenDown <ellenldowning@gmail.com>
2017-11-21 14:39:42 -0700

    font and style mini change

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="mainNav">
-    <nav class="navbar container">
+    <nav class="navbar">
         <div class="navbar-brand">
-          <a href="#" class="navbar-item column">
+          <div class="navbar-item column">
             <div class="name">
               <h1 class="logo">Ellen Louise Downing</h1>
             </div>
             <div class="tagline">
               <p>Creation Via Elagantly Crafted Code</p>
             </div>
-          </a>
+          </div>
           </div>
             <div class="navbar-end">
               <a class="navbar-item" @click.prevent="jump('#about')">About</a>
@@ -40,18 +40,29 @@ export default {
 <style scoped>
 
 nav {
-  margin-bottom: 5%;
+  margin-bottom:2rem;;
 }
 
 .navbar {
-  font-family: 'Codystar', cursive;
+  font-family: 'Poiret One', cursive;
   background-color: white;
   font-weight: bolder;
+  margin-left: 2rem;
+  margin-right: 2rem;
 }
 
 h1 {
-  font-size: 3rem;
+  font-size: 4rem;
+  text-transform: uppercase;
+  letter-spacing: .2rem;
 }
+
+a {
+  font-size: 1rem;
+  text-transform: uppercase;
+  letter-spacing: .2rem;
+}
+
 
 .tagline {
   font-size: 1.2rem;
